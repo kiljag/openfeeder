efc606008ae3ffb717fbafc65047f2079265845e
Jagadeesh <kjaga12dev@gmail.com>
2024-04-02 01:47:35 +0530

    minor db schema changes

--- db.sql
+++ db.sql
@@ -1,16 +1,16 @@
 
 CREATE TABLE feed (
     id BIGSERIAL PRIMARY KEY,
-    title TEXT,
-    url TEXT,
-    description TEXT
+    title TEXT NOT NULL,
+    url TEXT NOT NULL,
+    description TEXT NOT NULL
 );
 
 create TABLE feed_item (
     id BIGSERIAL PRIMARY KEY,
-    feed_id BIGINT REFERENCES feed(id),
-    title TEXT,
+    feed_id BIGINT NOT NULL REFERENCES feed(id),
+    title TEXT NOT NULL,
+    link TEXT NOT NULL,
     description TEXT,
-    link TEXT,
     pubDate TIMESTAMP
 );
